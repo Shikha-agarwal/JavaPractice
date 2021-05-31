dae7365381bca91cde6f2e6a997241b7c962adc2
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-05-31 12:50:50 +0530

    new challenge completed

--- src/SpeedConverter.java
+++ src/SpeedConverter.java
@@ -1,5 +1,3 @@
-import static java.lang.StrictMath.round;
-
 /**
  * Created By: Shikha Agarwal
  * Project Name: Java
@@ -10,19 +8,27 @@ public class SpeedConverter {
 
     public static void main(String[] args) {
 
-        long solvedAns = toMilesPerHour(25.42);
-        System.out.println(solvedAns);
+        printConversion(25.42);
+        printConversion(-12.43);
     }
 
     public static long toMilesPerHour(double kilometersPerHour){
 
         if(kilometersPerHour < 0){
             return -1;
-        } else if(kilometersPerHour > 0){
-            double kilometersInMiles = kilometersPerHour * 0.621;
-            return round(kilometersInMiles);
         }
-        return 0;
+            return Math.round(kilometersPerHour * 0.621);
+    }
+
+    public static void printConversion(double kilometersPerHour){
+
+        long convertedValue = toMilesPerHour(kilometersPerHour);
+
+        if(convertedValue == -1){
+            System.out.println("Invalid value");
+        } else {
+            System.out.println(kilometersPerHour + " km/h = " + convertedValue + " mi/h");
+        }
     }
 }
 
