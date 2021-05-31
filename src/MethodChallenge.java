735d4a2fb70a44773c3299f2214275cc366729af
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-05-31 11:56:07 +0530

    challenge completed

--- src/MethodChallenge.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created By: Shikha Agarwal
- * Project Name: Java
- * Package Name: PACKAGE_NAME
- * Date: 31-05-2021
- */
-public class MethodChallenge {
-}
-
---
+++ src/PositiveNegativeZero.java
@@ -0,0 +1,20 @@
+/**
+ * Created By: Shikha Agarwal
+ * Project Name: Java
+ * Package Name: PACKAGE_NAME
+ * Date: 31-05-2021
+ */
+public class PositiveNegativeZero {
+
+    public static void checkNumber(int number){
+
+        if(number > 0){
+            System.out.println("positive");
+        } else if(number < 0){
+            System.out.println("negative");
+        }else{
+            System.out.println("zero");
+        }
+    }
+}
+
