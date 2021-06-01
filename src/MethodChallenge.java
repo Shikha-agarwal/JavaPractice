016198ac039f6f78ecbaf9e823a32c78d81e494d
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-01 11:13:13 +0530

    Method Challenge

---
+++ src/MethodChallenge.java
@@ -0,0 +1,34 @@
+/**
+ * Created By: Shikha Agarwal
+ * Project Name: Java
+ * Package Name: PACKAGE_NAME
+ * Date: 01-06-2021
+ */
+public class MethodChallenge {
+
+    public static void main(String[] args) {
+
+        double inCentimeters = calcFeetAndInchesToCentimeters(72);
+        System.out.println(inCentimeters + " cm");
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches){
+
+        if((feet < 0) || (inches < 0) || (inches > 12)){
+            return -1;
+        }
+        return (feet * 12 * 2.54) + (inches * 2.54);
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches){
+
+        if(inches < 0){
+            return -1;
+        }
+        double feet = inches / 12 ;
+        double remaininginches = inches % 12;
+        return calcFeetAndInchesToCentimeters(feet ,remaininginches);
+    }
+}
+
+
