50d10e0a58cf6a1640483252b1f514a98ec461a6
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-01 14:38:44 +0530

    Simplified the code of the package playingCatChallenge;

--- src/playingCatChallenge/Main.java
+++ src/playingCatChallenge/Main.java
@@ -7,5 +7,14 @@ package playingCatChallenge;
  * Date: 01-06-2021
  */
 public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(PlayingCat.isCatPlaying(true, 10));
+
+        System.out.println(PlayingCat.isCatPlaying(false, 36));
+
+        System.out.println(PlayingCat.isCatPlaying(false, 35));
+    }
 }
 
--- src/playingCatChallenge/PlayingCat.java
+++ src/playingCatChallenge/PlayingCat.java
@@ -7,5 +7,14 @@ package playingCatChallenge;
  * Date: 01-06-2021
  */
 public class PlayingCat {
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+
+        if (summer) {
+           return (temperature >= 25) && (temperature <= 45);
+        } else {
+            return (temperature >= 25) && (temperature <= 35);
+        }
+    }
 }
 
