494528b5fb9a0d09137929f9e741c882b44819ba
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-01 18:43:35 +0530

    Added 3 more case in switch(value) of public class Switch

--- src/ControlFlowStatements/Switch.java
+++ src/ControlFlowStatements/Switch.java
@@ -26,6 +26,9 @@ public class Switch {
             case 2 :
                 System.out.println("Value was 2");
                 break;
+            case 3 : case 4 : case 5 :
+                System.out.println("Value was a 3, or a 4 or 5");
+                break;
             default:
                 System.out.println("Was not 1 or 2");
         }
