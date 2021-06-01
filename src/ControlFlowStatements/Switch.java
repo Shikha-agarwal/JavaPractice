7a1307e225e23c5d97e69b496cce535bd4456fee
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-01 18:36:15 +0530

    Written a code using switch case in public class Switch

--- src/ControlFlowStatements/Switch.java
+++ src/ControlFlowStatements/Switch.java
@@ -7,5 +7,30 @@ package ControlFlowStatements;
  * Date: 01-06-2021
  */
 public class Switch {
+
+    public static void main(String[] args) {
+        int value = 1;
+
+//        if (value == 1){
+//            System.out.println("Value was 1");
+//        } else if(value == 2){
+//            System.out.println("Value was 2");
+//        } else {
+//            System.out.println("Was not 1 or 2");
+//        }
+
+        switch (value){
+            case 1 :
+                System.out.println("Value was 1");
+                break;
+            case 2 :
+                System.out.println("Value was 2");
+                break;
+            default:
+                System.out.println("Was not 1 or 2");
+        }
+
+    }
 }
+
 
