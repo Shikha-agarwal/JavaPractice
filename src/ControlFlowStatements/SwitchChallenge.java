4a05aa39eb354e9ddf37cb706c48236765d386e2
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-01 19:01:34 +0530

    Created two switch statements: one for ichar and another for string in public class SwitchChallenge package ControlFlowStatements

--- src/ControlFlowStatements/SwitchChallenge.java
+++ src/ControlFlowStatements/SwitchChallenge.java
@@ -7,5 +7,27 @@ package ControlFlowStatements;
  * Date: 01-06-2021
  */
 public class SwitchChallenge {
+
+    public static void main(String[] args) {
+        char myChar = 'D';
+
+        switch (myChar) {
+            case 'A' : case 'B' : case 'C' : case 'D' : case 'E' :
+                System.out.println(myChar + " is found");
+                break;
+            default :
+                System.out.println("A,B,C,D or E is not foud");
+        }
+
+        String month = "January";
+
+        switch (month.toLowerCase()) {
+            case "january" : case "june" :
+                System.out.println(month);
+                break;
+            default:
+                System.out.println("Not sure");
+        }
+    }
 }
 
