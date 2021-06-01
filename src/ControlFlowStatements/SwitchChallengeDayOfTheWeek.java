36af4951b1dcd09fb51044f24375ffcda8204ed9
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-01 19:09:33 +0530

    Find the day of the week by using switch case in public class SwitchChallengeDayOfTheWeek in package ControlFlowStatements;

--- src/ControlFlowStatements/SwitchChallengeDayOfTheWeek.java
+++ src/ControlFlowStatements/SwitchChallengeDayOfTheWeek.java
@@ -7,5 +7,41 @@ package ControlFlowStatements;
  * Date: 01-06-2021
  */
 public class SwitchChallengeDayOfTheWeek {
+
+    public static void main(String[] args) {
+        printDayOfTheWeek(0);
+        printDayOfTheWeek(2);
+        printDayOfTheWeek(5);
+        printDayOfTheWeek(9);
+    }
+
+    public static void printDayOfTheWeek(int day) {
+
+        switch (day) {
+            case 0 :
+                System.out.println("Sunday");
+                break;
+            case 1 :
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6 :
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+    }
 }
 
