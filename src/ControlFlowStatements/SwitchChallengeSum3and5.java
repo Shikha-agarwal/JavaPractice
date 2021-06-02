97db2f57a3d4c59ec8106b4631d6a89d5a5bd88f
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-02 12:13:11 +0530

    Written the code for finding the sum of first five numbers divided by 3 and 5 in main method

--- src/ControlFlowStatements/SwitchChallengeSum3and5.java
+++ src/ControlFlowStatements/SwitchChallengeSum3and5.java
@@ -1,5 +1,4 @@
 package ControlFlowStatements;
-
 /**
  * Created By: Shikha Agarwal
  * Project Name: Java
@@ -8,7 +7,19 @@ package ControlFlowStatements;
  */
 public class SwitchChallengeSum3and5 {
     public static void main(String[] args) {
-
+        int count = 0;
+        int sum = 0;
+        for(int i = 1; i <= 1000; i++){
+            if((i % 3 == 0) && (i % 5 == 0)){
+                System.out.println(i);
+                sum = sum + i;
+                count = count + 1;
+                }
+            if(count == 5){
+                break;
+            }
+        }
+        System.out.println("The sum is: " + sum);
     }
 }
 
