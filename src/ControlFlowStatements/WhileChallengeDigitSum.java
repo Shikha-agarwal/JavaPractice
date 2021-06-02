47283c273e49e5f9062e9bfd4c154b84aade53bf
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-02 17:26:38 +0530

    Created a new method  public static int sumDigits(int number) in  public class WhileChallengeDigitSum.java that will return the sum of didgits of the number passed as parameter.

--- src/ControlFlowStatements/WhileChallengeDigitSum.java
+++ src/ControlFlowStatements/WhileChallengeDigitSum.java
@@ -8,7 +8,22 @@ package ControlFlowStatements;
  */
 public class WhileChallengeDigitSum {
     public static void main(String[] args) {
+        System.out.println(sumDigits(125));
+        System.out.println(sumDigits(9));
+        System.out.println(sumDigits(-125));
+        System.out.println(sumDigits(36925));
+    }
 
+    public static int sumDigits(int number) {
+        int sum = 0;
+        if( number >= 10){
+            while(number != 0){
+                sum += (number % 10);
+                number /= 10;
+            }
+            return sum;
+        }
+        return -1;
     }
 }
 
