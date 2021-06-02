d8a0c36b1d35a99b0a406fe50e76c1f5db5289d2
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-02 18:00:21 +0530

    Used Integer.parseInt(); to convert a string value into integer value

--- src/ParseString.java
+++ src/ParseString.java
@@ -6,7 +6,17 @@
  */
 public class ParseString {
     public static void main(String[] args) {
+        String myStringValue = "2020";
+        System.out.println("numberAsString = " + myStringValue);
 
+        // converting String into int
+        int number = Integer.parseInt(myStringValue);
+        System.out.println("number = " + number);
+
+        myStringValue += 1;                 // 20201
+        number += 1;                        // 2020
+        System.out.println("numberAsString = " + myStringValue);
+        System.out.println("number = " + number);
     }
 }
 
