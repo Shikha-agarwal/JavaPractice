ab19623927a294c581e8195f52221c9481c7d59b
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-02 22:01:20 +0530

    Created 2 methods public static void main(String[] args) public static int getEvenDigitSum(int number) in class public class WhileChallengeEvenDigitSum.java

---
+++ src/ControlFlowStatements/WhileChallengeEvenDigitSum.java
@@ -0,0 +1,18 @@
+package ControlFlowStatements;
+
+/**
+ * Created By: Shikha Agarwal
+ * Project Name: Java
+ * Package Name: ControlFlowStatements
+ * Date: 02-06-2021
+ */
+public class WhileChallengeEvenDigitSum {
+    public static void main(String[] args) {
+
+    }
+
+    public static int getEvenDigitSum(int number) {
+       return 0;
+    }
+}
+
