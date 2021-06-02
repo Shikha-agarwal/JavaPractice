661e7c1ac923212c814dd67d44796d5c91e7ac0f
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-02 13:06:25 +0530

    Created 3 methods: public static void main(String[] args) public static boolean isOdd(int number)  public static int sumOdd(int start, int end)  in  public class ForChallengeSumOdd in package ControlFlowStatements,  for calculating the sum of odd numbers in a particular range

--- src/ControlFlowStatements/ForChallengeSumOdd.java
+++ src/ControlFlowStatements/ForChallengeSumOdd.java
@@ -7,5 +7,34 @@ package ControlFlowStatements;
  * Date: 02-06-2021
  */
 public class ForChallengeSumOdd {
+
+    public static void main(String[] args) {
+        System.out.println(sumOdd(1,100));
+        System.out.println(sumOdd(-1,100));
+        System.out.println(sumOdd(100,100));
+        System.out.println(sumOdd(13,13));
+        System.out.println(sumOdd(100,-100));
+        System.out.println(sumOdd(100,1000));
+    }
+
+    public static boolean isOdd(int number) {
+        if(number < 0){
+            return false;
+        }
+        return (number % 2 != 0);
+    }
+
+    public static int sumOdd(int start, int end) {
+        if(end < start || start < 0){
+            return -1;
+        }
+        int sum = 0;
+        for(int i = start; i <= end; i ++){
+            if(isOdd(i)){
+                sum += i;
+            }
+        }
+        return sum;
+    }
 }
 
