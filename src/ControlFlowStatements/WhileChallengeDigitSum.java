403ef1500d41a81fccb7076dc0c9fdec07471aee
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-02 17:17:43 +0530

    Created a public class WhileChallengeDigitSum.java with a main method public static void main(String args[])

---
+++ src/ControlFlowStatements/WhileChallengeDigitSum.java
@@ -0,0 +1,15 @@
+package ControlFlowStatements;
+
+/**
+ * Created By: Shikha Agarwal
+ * Project Name: Java
+ * Package Name: ControlFlowStatements
+ * Date: 02-06-2021
+ */
+public class WhileChallengeDigitSum {
+    public static void main(String[] args) {
+
+    }
+}
+
+
