21fabf2926a69d72bc9113f07368338049c7db29
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-02 17:54:54 +0530

    Created a new java class public class ParseString.java with a main method public static void main(String args[])

---
+++ src/ParseString.java
@@ -0,0 +1,12 @@
+/**
+ * Created By: Shikha Agarwal
+ * Project Name: Java
+ * Package Name: PACKAGE_NAME
+ * Date: 02-06-2021
+ */
+public class ParseString {
+    public static void main(String[] args) {
+
+    }
+}
+
