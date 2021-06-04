f123afd805f2d8e09dcac9673499f7f50f246045
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-04 17:32:28 +0530

    Created a new java class public class LargestPrimeFactor.java with 2 methods  public static void main(String[] args) : which calls public static int getLargestPrime(int number) method
     public static int getLargestPrime(int number): which returns the highest prime factor of the number

---
+++ src/ControlFlowStatements/LargestPrimeFactor.java
@@ -0,0 +1,43 @@
+package ControlFlowStatements;
+
+/**
+ * Created By: Shikha Agarwal
+ * Project Name: Java
+ * Package Name: ControlFlowStatements
+ * Date: 04-06-2021
+ */
+public class LargestPrimeFactor {
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(21));
+        System.out.println(getLargestPrime(217));
+        System.out.println(getLargestPrime(0));
+        System.out.println(getLargestPrime(45));
+        System.out.println(getLargestPrime(-1));
+    }
+
+    public static int getLargestPrime(int number) {
+        if(number <= 0){
+            return -1;
+        }
+        int largestPrime = 0;
+        int j;
+        for(int i = 2; i < number; i++) {
+            if(number % i == 0){
+                for( j = 2; j < i; j++){
+                    if(i % j == 0){
+                        break;
+                    }
+                }
+                if( j == i){
+                    largestPrime = i;
+                }
+            }
+        }
+        if(largestPrime == 0){
+            return -1;
+        }
+        return largestPrime;
+    }
+}
+
+
