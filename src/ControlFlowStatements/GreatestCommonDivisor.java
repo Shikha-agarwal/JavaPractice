d5d9f7384325c14ea10562603a06f8da8fc0437c
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-04 12:42:43 +0530

    Created a two methods: public static void main(String[] args): which calls the public static int getGreatestCommonDivisor(int first, int second)
    public static int getGreatestCommonDivisor(int first, int second) that returns the gretest common divisor of the two numbers passed
    in java class
    public class GreatestCommonDivisor.java

--- src/ControlFlowStatements/GreatestCommonDivisor.java
+++ src/ControlFlowStatements/GreatestCommonDivisor.java
@@ -7,5 +7,25 @@ package ControlFlowStatements;
  * Date: 04-06-2021
  */
 public class GreatestCommonDivisor {
+    public static void main(String[] args) {
+        System.out.println(getGreatestCommonDivisor(25 ,15));
+        System.out.println(getGreatestCommonDivisor(12, 30));
+        System.out.println(getGreatestCommonDivisor(9, 18));
+    }
+
+    public static int getGreatestCommonDivisor(int first, int second) {
+        if((first < 10) || (second < 10)){
+            return -1;
+        }
+        int value;
+        int greatest = 1;
+        value = Math.max(first, second);
+        for(int i = 2; i <= value; i++){
+            if((first % i == 0) && (second % i == 0)){
+               greatest = i;
+            }
+        }
+        return  greatest;
+    }
 }
 
