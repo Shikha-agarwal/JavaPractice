0fd5acccae6ce673be3fe6196f5404584b9a761d
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-04 17:59:37 +0530

    Created a new java class public class DiagonalStar.java with 2 methods public static void main(String[] args) and public static void printSquareStar(int number)

---
+++ src/ControlFlowStatements/DiagonalStar.java
@@ -0,0 +1,21 @@
+package ControlFlowStatements;
+
+/**
+ * Created By: Shikha Agarwal
+ * Project Name: Java
+ * Package Name: ControlFlowStatements
+ * Date: 04-06-2021
+ */
+public class DiagonalStar {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printSquareStar(int number) {
+        if(number < 5) {
+            System.out.println("Invalid Value");
+        }
+
+    }
+}
+
