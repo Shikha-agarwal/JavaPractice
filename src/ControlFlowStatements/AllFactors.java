455b663c2fbe777634bddfdbcbb09e212eb2b4cb
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-04 13:04:27 +0530

    Created a class public class AllFactors.java with two methods  public static void main(String[] args): which calls the  public static void printFactors(int number) method
      public static void printFactors(int number): which prints all the factors of the number passed as parameter.

--- src/ControlFlowStatements/AllFactors.java
+++ src/ControlFlowStatements/AllFactors.java
@@ -7,6 +7,27 @@ package ControlFlowStatements;
  * Date: 04-06-2021
  */
 public class AllFactors {
+    public static void main(String[] args) {
+        printFactors(6);
+        System.out.println("-----------------------------------");
+        printFactors(32);
+        System.out.println("-----------------------------------");
+        printFactors(10);
+        System.out.println("-----------------------------------");
+        printFactors(-1);
+    }
 
+    public static void printFactors(int number){
+        if(number < 1){
+            System.out.println("Invalid Value");
+        }
+        int i = 1;
+        while(i <= number){
+            if(number % i == 0){
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
 }
 
