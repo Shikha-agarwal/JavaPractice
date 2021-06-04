5ff059ed7b1d13c193923aad11332e0b678e5b51
Shikha-agarwal <shikha.agarwal_cs19@gla.ac.in>
2021-06-04 13:13:59 +0530

    Created a new java class public class WhileChallengePerfectNumber.java That has two methods  public static void main(String[] args): which prints the value by calling the method public static boolean isPerfectNumber(int number)
     public static boolean isPerfectNumber(int number): which returns that whether the number is perfect or not.

---
+++ src/WhileChallengePerfectNumber.java
@@ -0,0 +1,30 @@
+/**
+ * Created By: Shikha Agarwal
+ * Project Name: Java
+ * Package Name: PACKAGE_NAME
+ * Date: 04-06-2021
+ */
+public class WhileChallengePerfectNumber {
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        if(number < 1){
+            return false;
+        }
+        int sum = 0;
+        int i = 1;
+        while(i < number) {
+            if(number % i == 0){
+                sum += i;
+            }
+            i++;
+        }
+        return (sum == number);
+    }
+}
+
